32b079b7b8f549169f75b3544df0edaa1d307661
luminousxv <drgvyhn@gmail.com>
2022-07-06 18:55:54 +0900

    React Navigation에 필요한 타입 선언 Drawer Navigation Container들의 파라미터 타입 선언 DrawerScreenProps를 활용한 Props 타입 선언 App.tsx에 쓰일 navigation을 위한 HomeScreenNavigationProp 선언

---
+++ Home_Sensor/src/types/navigation/navigationTypes.ts
@@ -0,0 +1,20 @@
+import {DrawerScreenProps} from '@react-navigation/drawer';
+
+export type RootDrawerParamList = {
+  Home: {
+    temperature: number;
+    co2: number;
+    humidity: number;
+    realtemp: number;
+    realhumid: number;
+  };
+  온도: undefined;
+  습도: undefined;
+  종합공기질: undefined;
+};
+
+export type Props = DrawerScreenProps<RootDrawerParamList, 'Home'>;
+
+export type HomeScreenNavigationProp = Props['navigation'];
+
+export type HomeScreenRouteProp = Props['route'];
